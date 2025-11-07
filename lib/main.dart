aced903c153091eafa6235acaa2c0d59c887aff1
abday_hafidz <abday.hafidz23@gmail.com>
2025-11-07 11:25:01 +0700

    memodifikasi file main dan menambahkan nama serta nim, dan mengubah colorseed menjadi BlueGrey

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screen/splash_screen_dart1.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'NIM : 1123150093, Muhammad abday abdul hafidz',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF607D8B)
+        ),
+      ),
+      home: SplashScreenDart1(),
+    );
+  }
+}
