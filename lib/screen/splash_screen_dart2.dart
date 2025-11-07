eb5b63c17a0f36fc323bc8a7b2baaa733ce10feb
abday_hafidz <abday.hafidz23@gmail.com>
2025-11-07 12:14:08 +0700

    mulai ngerjain splash 2

--- lib/screen/splash_screen_dart2.dart
+++ lib/screen/splash_screen_dart2.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
-import 'splash_screen_dart3.dart';
+import 'package:kb1179_1123150093_uts/screen/splash_screen_dart3.dart';
+
 
 class SplashScreenDart2 extends StatelessWidget {
   const SplashScreenDart2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+            appBar: AppBar(
+              title: const Text("Dashboard"),
+              actions: const [],
+            ),
+            body: SingleChildScrollView(
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                child: const Column(
+                  children: [],
+                ),
+              ),
+            ),
+          );
   }
 }
