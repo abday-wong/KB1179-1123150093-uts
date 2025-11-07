8cb21fa67f1b8b9596320b36389220a7b77f7678
abday_hafidz <abday.hafidz23@gmail.com>
2025-11-07 12:28:49 +0700

    salin splash 1 dan ubah detail

--- lib/screen/splash_screen_dart2.dart
+++ lib/screen/splash_screen_dart2.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kb1179_1123150093_uts/screen/splash_screen_dart3.dart';
-
+import 'package:kb1179_1123150093_uts/screen/login.dart';
+import '../main.dart';
 
 class SplashScreenDart2 extends StatelessWidget {
   const SplashScreenDart2({super.key});
@@ -8,18 +9,105 @@ class SplashScreenDart2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            appBar: AppBar(
-              title: const Text("Dashboard"),
-              actions: const [],
-            ),
-            body: SingleChildScrollView(
-              child: Container(
-                padding: const EdgeInsets.all(20.0),
-                child: const Column(
-                  children: [],
-                ),
+      appBar: AppBar(actions: []),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              SizedBox(height: 50,),
+              Container(
+                width: 250,
+                    height: 250,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.blueGrey,
+                      image: DecorationImage(image: 
+                      AssetImage("assets/images/epwang.png"),
+                      fit: BoxFit.cover
+                      ),
+                      ),
               ),
-            ),
-          );
+              Text('pencet lanjut buat lanjut',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                    SizedBox(height: 30),
+                    Text('Barang siapa yang rajin solat\nistrinya ashley\nashley nya ada 5',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                    SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 5,
+                          height: 5,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Container(
+                          width: 5,
+                          height: 5,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Container(
+                          width: 5,
+                          height: 5,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey[100]!,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 30),
+                    Container(
+                      margin: EdgeInsets.only(left: 20, right: 20),
+                      child: SizedBox(
+                        height: 25 ,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            //next todo
+                            Navigator.push(context,
+                             MaterialPageRoute(builder: (context) => SplashScreenDart2()),
+                             );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blueGrey
+                          ),
+                      
+                          child: Text('Lanjut coy', 
+                          style: TextStyle(
+                            fontSize: 15,
+                          color: Colors.white
+                          ),
+                          ),
+                          
+                          ),
+                      ),
+                    ), 
+                    Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screen/splash_screen_dart3.dart
+++ lib/screen/splash_screen_dart3.dart
@@ -1,10 +1,111 @@
 import 'package:flutter/material.dart';
+import 'splash_screen_dart2.dart';
 
-class SplashScreenDart3 extends StatelessWidget {
-  const SplashScreenDart3({super.key});
+class SplashScreenDart1 extends StatelessWidget {
+  const SplashScreenDart1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(actions: []),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              SizedBox(height: 50,),
+              Container(
+                width: 250,
+                    height: 250,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.blueGrey,
+                      image: DecorationImage(image: 
+                      AssetImage("assets/images/jarvis.png"),
+                      fit: BoxFit.cover
+                      ),
+                      ),
+              ),
+              Text('Welcome selamat datang',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                    SizedBox(height: 30),
+                    Text('Barang siapa yang rajin solat\nistrinya ada wong\nada wong nya 5',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                    SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 5,
+                          height: 5,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Container(
+                          width: 5,
+                          height: 5,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey[100]!,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Container(
+                          width: 5,
+                          height: 5,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey[100]!,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 30),
+                    Container(
+                      margin: EdgeInsets.only(left: 20, right: 20),
+                      child: SizedBox(
+                        height: 25 ,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            //next todo
+                            Navigator.push(context,
+                             MaterialPageRoute(builder: (context) => SplashScreenDart2()),
+                             );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blueGrey
+                          ),
+                      
+                          child: Text('Lanjut coy', 
+                          style: TextStyle(
+                            fontSize: 15,
+                          color: Colors.white
+                          ),
+                          ),
+                          
+                          ),
+                      ),
+                    ), 
+                    Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
